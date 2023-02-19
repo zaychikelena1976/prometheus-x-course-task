fc5b736d1a1c4e53f4a1b03172b8fee5d2cc4de1
Olena Lomonos <zaychikelena1976@gmail.com>
2023-02-19 09:34:56 +0200

    change basket

--- src/pages/basket/basket.jsx
+++ src/pages/basket/basket.jsx
@@ -4,18 +4,17 @@ import React from "react";
 export default function Basket() {
   return (
     <>
-      
       <div className="wrapper">
         <div className="row">
           <div className="col-xs-5 col-md-5 col-sm-5 "></div>
           <div className="col-xs-5 col-md-5 col-sm-5 "></div>
           <div className="col-xs-2 col-md-2 col-sm-2 border-2 text-end">
-            <button
+            {/* <button
               className="btn-sm btn-warning border rounded text-primary"
               type="button"
             >
               Purchase
-            </button>
+            </button> */}
           </div>
           <div className="col-xs-4 col-md-4 col-sm-4"></div>
           <div className="col-xs-4 col-md-4 col-sm-4">
@@ -23,7 +22,6 @@ export default function Basket() {
           </div>
         </div>
       </div>
-      
     </>
   );
 }
